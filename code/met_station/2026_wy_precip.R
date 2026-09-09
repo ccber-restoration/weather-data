d173e2cb651823e419cb010325347f9b4c161c5f
fjoyce <fhjoyce@ucsb.edu>
2026-09-09 12:01:16 -0700

    start rewriting script with data for full 2026 water year, and using locally-defined water year for SB county (starts in Sep instead of Oct)

--- code/met_station/2026_wy_precip.R
+++ code/met_station/2026_wy_precip.R
@@ -26,33 +26,41 @@ dowy = function(x, start.month = 10L){
   as.integer(x - start.date + 1L)
 }
 
+# define function for calculating day of LOCAL water year (starting in September instead of October)
+dolwy = function(x, start.month = 9L){
+  start.yr = year(x) - (month(x) < start.month)
+  start.date = make_date(start.yr, start.month, 1L)
+  as.integer(x - start.date + 1L)
+}
+
+
 #test day of water year (dowy) function
-dowy(as.Date("2025-09-30"))
+dolwy(as.Date("2025-09-30"))
 
 #read in NOAA daily summaries from 2008-08-14 through 2025-12-06
-daily_summaries <- read_csv(file = "data/NOAA_weather_station/NOAA_daily_summaries_USW00053152_full_2025-12-09.csv") %>% 
+daily_summaries <- read_csv(file = "data/NOAA_weather_station/NOAA_daily_summaries_USW00053152_full_2026-09-05.csv") %>% 
   clean_names() %>% 
   #create columns for month and year
   mutate(year = year(date),
          month = month(date),
          day = day(date),
          #create column for "day of water year"
-         dowy = dowy(date)) %>% 
+         dolwy = dolwy(date)) %>% 
   #create column for wateryear
   mutate(wy = case_when(
-    month >= 10 ~ year + 1,
+    month >= 9 ~ year + 1,
     .default = year
   ))
 
 #create data frame for 2026 water year
 #(first 67 days of 2026 water year)
 water_year_2026 <- daily_summaries %>% 
-  filter(date > "2025-09-30" & date < "2026-10-01") %>% 
+  filter(date > "2025-08-31" & date < "2026-09-01") %>% 
   mutate(cumulative_prcp = cumsum(prcp))
 
 #check for missing dates 
-start_date <- as.Date("2025-10-01")
-end_date <- as.Date("2026-09-30")
+start_date <- as.Date("2025-09-01")
+end_date <- as.Date("2026-08-31")
 all_expected_dates <- seq(from = start_date, to = end_date, by = "day")
 
 missing_date <- setdiff(all_expected_dates, water_year_2026$date)
@@ -63,12 +71,12 @@ as.Date(missing_date)
 rainy_days <- water_year_2026 %>% 
   filter(prcp >= 0.1)
 
-#[8] days with more than 0.1 inch of rain
+#[24] days with more than 0.1 inch of rain
 
 very_wet_days <- water_year_2026 %>% 
   filter(prcp >= 1)
 
-#[3] days with more than 1 inch of rain
+#[7] days with more than 1 inch of rain
 
 #single rainiest day was [2025-11-14] with [2.57] inches of rain
 
